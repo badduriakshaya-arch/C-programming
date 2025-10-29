2220271d430a01f5e47db561a9e38438c80d5a64
badduriakshaya-arch <badduriakshaya@gmail.com>
2025-10-29 18:52:24 +0530

    Add files via upload

---
+++ area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int l,b,area;
+	printf("enter l value");
+	scanf("%d",&l);
+	printf("enter b value");
+	scanf("%d",&b);
+	area=l*b;
+	printf("area=%d",area);
+	return 0;
+}
