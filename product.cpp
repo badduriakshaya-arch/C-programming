c7e595726ef7a62fce2a8cc58e03cda6f9ac794a
badduriakshaya-arch <badduriakshaya@gmail.com>
2025-10-29 18:42:41 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,product;
+	printf("enter a value");
+	scanf("%d",&a);
+	printf("enter b value");
+	scanf("%d",&b);
+	product=a*b;
+	printf("product=%d",product);
+	return 0;
+}
