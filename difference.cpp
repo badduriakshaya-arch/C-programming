ed4765f06fb68a4131a337a3dab726e3ccd5cb68
badduriakshaya-arch <badduriakshaya@gmail.com>
2025-10-29 18:41:09 +0530

    Add files via upload

---
+++ difference.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,difference;
+	printf("enter a value");
+	scanf("%d",&a);
+	printf("enter b value");
+	scanf("%d",&b);
+	difference=a-b;
+	printf("difference=%d",difference);
+	return 0;
+}
