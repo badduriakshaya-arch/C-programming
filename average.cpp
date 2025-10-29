d2ac646279d85715181891a631c71ed992ca9cc7
badduriakshaya-arch <badduriakshaya@gmail.com>
2025-10-29 18:51:29 +0530

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,avg;
+	printf("enter a value");
+	scanf("%d",&a);
+	printf("enter b value");
+	scanf("%d",&b);
+	printf("enter c value");
+	scanf("%d",&c);
+	avg=(a+b+c)/3;
+	printf("avg=%d",avg);
+	return 0;
+}
