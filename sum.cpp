a4af46e6305dd0de263ca0aa8171c5c5b55933c6
badduriakshaya-arch <badduriakshaya@gmail.com>
2025-10-30 13:10:11 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("enter a value");
+	scanf("%d",&a);
+	printf("enter b value");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("sum of two numbers=%d",sum);
+	return 0;
+}
