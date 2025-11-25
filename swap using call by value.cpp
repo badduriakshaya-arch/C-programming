d1c53b2aa94eee7c48af5004c3f92186bae84249
badduriakshaya-arch <badduriakshaya@gmail.com>
2025-11-25 15:48:51 +0530

    Add files via upload

---
+++ swap using call by value.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void swap(int,int);
+int main()
+{
+	int a,b;
+	printf("enter a and b values");
+	scanf("%d%d",&a,&b);
+	printf("\n before swap a=%db=%d",a,b);
+	swap(a,b);
+	return 0;
+}
+void swap(int x,int y)
+{
+	int temp;
+	temp=x;
+	x=y;
+	y=temp;
+	printf("\n after swap a=%db=%d",x,y);
+}
