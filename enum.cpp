13d762b139270364f7f20c48949767884bd84fe8
badduriakshaya-arch <badduriakshaya@gmail.com>
2026-04-14 19:34:32 +0530

    Add files via upload

---
+++ enum.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	enum week{mon=1,tue,wed,thu,fri,sat,sun};
+	printf("tue=%d",tue);
+	printf("fri=%d",fri);
+	printf("sun=%d",sun);
+	return 0;
+}
